193e97fbd2d010f3b0e94696250fe70d77e1f07b
Sam Handelsman <samhandelsman@gmail.com>
2023-08-25 19:23:07 -0400

    updated shoppingcart routes

--- app/api/shopping_cart_routes.py
+++ app/api/shopping_cart_routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint
 from flask_login import login_required
-from app.models import shopping_cart
+from app.models import ShoppingCart
 
 shopping_cart_routes = Blueprint('shopping_carts', __name__)
 
@@ -11,15 +11,15 @@ def shopping_carts():
     """
     Query for all shopping_carts and returns them in a list of shopping_cart dictionaries
     """
-    shopping_carts = shopping_cart.query.all()
-    return {'shopping_carts': [shopping_cart.to_dict() for shopping_cart in shopping_carts]}
+    shopping_carts = ShoppingCart.query.all()
+    return {'shopping_carts': [cart.to_dict() for cart in shopping_carts]}
 
 
 @shopping_cart_routes.route('/<int:id>')
 @login_required
-def shopping_cart(id):
+def shopping_carts(id):
     """
     Query for a shopping_cart by id and returns that shopping_cart in a dictionary
     """
-    shopping_cart = shopping_cart.query.get(id)
+    shopping_cart = ShoppingCart.query.get(id)
     return shopping_cart.to_dict()
