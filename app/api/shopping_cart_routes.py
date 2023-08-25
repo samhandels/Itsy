0b4a6afb27694f3eed32d0b45ee423c7114018ce
Sam Handelsman <samhandelsman@gmail.com>
2023-08-25 19:28:56 -0400

    sorry shopping cart

--- app/api/shopping_cart_routes.py
+++ app/api/shopping_cart_routes.py
@@ -1,25 +1,25 @@
-from flask import Blueprint
-from flask_login import login_required
-from app.models import ShoppingCart
+# from flask import Blueprint
+# from flask_login import login_required
+# from app.models import ShoppingCart
 
-shopping_cart_routes = Blueprint('shopping_carts', __name__)
-
-
-@shopping_cart_routes.route('/')
-@login_required
-def shopping_carts():
-    """
-    Query for all shopping_carts and returns them in a list of shopping_cart dictionaries
-    """
-    shopping_carts = ShoppingCart.query.all()
-    return {'shopping_carts': [cart.to_dict() for cart in shopping_carts]}
+# shopping_cart_routes = Blueprint('shopping_carts', __name__)
 
 
-@shopping_cart_routes.route('/<int:id>')
-@login_required
-def shopping_carts(id):
-    """
-    Query for a shopping_cart by id and returns that shopping_cart in a dictionary
-    """
-    shopping_cart = ShoppingCart.query.get(id)
-    return shopping_cart.to_dict()
+# @shopping_cart_routes.route('/')
+# @login_required
+# def shopping_carts():
+#     """
+#     Query for all shopping_carts and returns them in a list of shopping_cart dictionaries
+#     """
+#     shopping_carts = ShoppingCart.query.all()
+#     return {'shopping_carts': [cart.to_dict() for cart in shopping_carts]}
+
+
+# @shopping_cart_routes.route('/<int:id>')
+# @login_required
+# def shopping_carts(id):
+#     """
+#     Query for a shopping_cart by id and returns that shopping_cart in a dictionary
+#     """
+#     shopping_cart = ShoppingCart.query.get(id)
+#     return shopping_cart.to_dict()
