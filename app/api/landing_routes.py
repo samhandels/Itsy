7942fd159b771319f7e79611b1362597f59962a3
Aila Lu <ailalutw@gmail.com>
2023-08-26 14:09:40 -0700

    gett all shopping cart info

--- app/api/landing_routes.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, jsonify
-
-landing_routes = Blueprint('landing', __name__)
-
-@landing_routes.route('/')
-def landing():
-    return ("<h1>hello world</h1>")
--- app/api/shopping_cart_routes.py
+++ app/api/shopping_cart_routes.py
@@ -1,7 +1,6 @@
 from flask import Blueprint, request
 from flask_login import login_required, current_user
 from ..models.shopping_cart_items import ShoppingCartItems
-from ..forms.shopping_cart_form import ShoppingCartForm
 
 shopping_cart = Blueprint('shopping_carts', __name__)
 
@@ -10,24 +9,24 @@ shopping_cart = Blueprint('shopping_carts', __name__)
 @login_required
 def get_shopping_cart():
     """
-    Query for all shopping_cart_items and returns them in a list of shopping_cart dictionaries
+    Query for all shopping_cart_items and returns them in a list of shopping_cart dictionaries with the current user
     """
-    shopping_cart = ShoppingCartItems.query.all()
+    ShoppingCartItems.query.filter(ShoppingCartItems.shoppingCartId == current_user.id).all()
     response = [cart.to_dict() for cart in shopping_cart]
     print(response)
     return {'shopping_carts': response}
 
 
 
-#this goes into products detail page
-@products.route('products/<int:id>', methods=["POST"])
-@login_required
-def create_shopping_cart_item_by_product():
-    """
-    Create a shopping cart item to the shopping cart from the product detail page
-    """
-    shopping_cart = ShoppingCartItems.query.get(id)
-    return shopping_cart.to_dict()
+# #this goes into products detail page
+# @products.route('products/<int:id>', methods=["POST"])
+# @login_required
+# def create_shopping_cart_item_by_product():
+#     """
+#     Create a shopping cart item to the shopping cart from the product detail page
+#     """
+#     shopping_cart = ShoppingCartItems.query.get(id)
+#     return shopping_cart.to_dict()
 
 
 
