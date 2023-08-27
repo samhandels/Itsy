d357dad045c64d0dd2fb25185384cdc137266d52
Sam Handelsman <samhandelsman@gmail.com>
2023-08-27 16:43:31 -0700

    added cool juvie to pic

--- react-app/src/components/Footer/index.js
+++ react-app/src/components/Footer/index.js
@@ -19,7 +19,7 @@ function Footer() {
                         <p>Aila</p>
                     </div>
                     <div className="team-member">
-                        <img src="https://media.licdn.com/dms/image/C4E03AQHw8P2euo3WZQ/profile-displayphoto-shrink_200_200/0/1517638025526?e=1698883200&v=beta&t=lnYjzvYCaP07-P7VWIQKpe-kwv-p3pCzFJhImAhd2fs" alt="Team Member 2" />
+                        <img src="https://media.licdn.com/dms/image/C4D03AQFtc-pVwmDnEA/profile-displayphoto-shrink_200_200/0/1604125040371?e=1698883200&v=beta&t=hqwEc_8bV5b3XfhXlBlttQoiGcn9hyuv6bcTH9624T4" alt="Team Member 2" />
                         <p>Juvenal</p>
                     </div>
                     <div className="team-member">
