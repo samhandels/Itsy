cf855ddc599256bd393901839bc4f7cde50bde1b
Sam Handelsman <samhandelsman@gmail.com>
2023-08-27 15:55:17 -0700

    updated footer

--- react-app/src/components/Footer/index.js
+++ react-app/src/components/Footer/index.js
@@ -1,19 +1,49 @@
 import React from 'react';
 import './Footer.css';
 
-function Footer(){
-	return (
-		<footer className="footer-container">
-			<div>
-				<p>© 2023 Itsy</p>
-			</div>
-			<div>
-				<a href="/terms">Terms & Conditions</a>
-				<a href="/privacy">Privacy Policy</a>
-				<a href="/contact">Contact Us</a>
-			</div>
-		</footer>
-	);
+function Footer() {
+    return (
+        <footer className="footer-container">
+
+            {/* Renewable energy section */}
+            <div className="renewable-energy-section">
+                <p>Itsy is powered by 100% renewable energy</p>
+            </div>
+
+            {/* Who we are section */}
+            <div className="team-section">
+                <h2>Who We Are:</h2>
+                <div className="team-members">
+                    <div className="team-member">
+                        <img src="https://media.licdn.com/dms/image/C4E03AQHw8P2euo3WZQ/profile-displayphoto-shrink_200_200/0/1517638025526?e=1698883200&v=beta&t=lnYjzvYCaP07-P7VWIQKpe-kwv-p3pCzFJhImAhd2fs" alt="Team Member 1" />
+                        <p>Aila</p>
+                    </div>
+                    <div className="team-member">
+                        <img src="/path/to/headshot2.jpg" alt="Team Member 2" />
+                        <p>Juvenal</p>
+                    </div>
+                    <div className="team-member">
+                        <img src="https://media.licdn.com/dms/image/C5603AQF0GSgWR49DHw/profile-displayphoto-shrink_200_200/0/1568408227822?e=1698883200&v=beta&t=lqj5Bb0FKkdi2f-MsDgd8aVbgyfvPwg8kiMSLp1N4Mg" alt="Team Member 3" />
+                        <p>Sam</p>
+                    </div>
+                    <div className="team-member">
+                        <img src="/path/to/headshot4.jpg" alt="Team Member 4" />
+                        <p>Erica</p>
+                    </div>
+                </div>
+            </div>
+
+            {/* Bottom links section */}
+            <div className="bottom-links">
+                <p>© 2023 Itsy</p>
+                <div className="links">
+                    <a href="/terms">Terms & Conditions</a>
+                    <a href="/privacy">Privacy Policy</a>
+                    <a href="/contact">Contact Us</a>
+                </div>
+            </div>
+        </footer>
+    );
 }
 
 export default Footer;
