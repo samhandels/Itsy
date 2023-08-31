1bcfb8c3481fbd0c4d86b20ad84b77632521c724
Erica Zimmerman <ericanzimmerman@gmail.com>
2023-08-31 11:26:11 -0500

    added likes column to reviews table

--- app/seeds/reviews.py
+++ app/seeds/reviews.py
@@ -3,28 +3,36 @@ from sqlalchemy.sql import text
 
 def seed_reviews():
     review1 = Review(
-        productId = 1, userId = 2, review="Here is a review for product 1 by user 2", stars = 1
+        productId = 1, userId = 2, review="Here is a review for product 1 by user 2", stars = 1,
+        likes = 0
     )
     review2 = Review(
-        productId = 2, userId = 3, review="Here is a review for product 2 by user 3", stars = 2
+        productId = 2, userId = 3, review="Here is a review for product 2 by user 3", stars = 2,
+        likes = 0
     )
     review3= Review(
-        productId = 3, userId = 1, review="Here is a review for product 3 by user 1", stars = 3
+        productId = 3, userId = 1, review="Here is a review for product 3 by user 1", stars = 3,
+        likes = 0
     )
     review4= Review(
-        productId = 4, userId = 2, review="Here is a review for product 4 by user 2", stars = 4
+        productId = 4, userId = 2, review="Here is a review for product 4 by user 2", stars = 4,
+        likes = 0
     )
     review5= Review(
-        productId = 5, userId = 3, review="Here is a review for product 5 by user 3", stars = 5
+        productId = 5, userId = 3, review="Here is a review for product 5 by user 3", stars = 5,
+        likes = 0
     )
     review6= Review(
-        productId = 6, userId = 1, review="Here is a review for product 6 by user 1", stars = 4
+        productId = 6, userId = 1, review="Here is a review for product 6 by user 1", stars = 4,
+        likes = 0
     )
     review7= Review(
-        productId = 7, userId = 3, review="Here is a review for product 7 by user 3", stars = 3
+        productId = 7, userId = 3, review="Here is a review for product 7 by user 3", stars = 3,
+        likes = 0
     )
     review8= Review(
-        productId = 8, userId = 1, review="Here is a review for product 8 by user 1", stars = 2
+        productId = 8, userId = 1, review="Here is a review for product 8 by user 1", stars = 2,
+        likes = 0
     )
 
     db.session.add(review1)
