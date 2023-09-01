e398563e52c2fa61fe0a062732eebb21cea9be10
Erica Zimmerman <ericanzimmerman@gmail.com>
2023-08-31 20:53:00 -0500

    fixing index

--- react-app/src/components/ShoppingCart/OrderCompleteModal/index.js
+++ react-app/src/components/ShoppingCart/OrderCompleteModal/index.js
@@ -9,11 +9,21 @@ import { deleteItemThunk } from "../../../store/shoppingCartReducer";
 // need to update the products quantity after the order is complete
 import { fetchProducts, fetchUpdateProduct } from "../../../store/productsReducer";
 
-export function OrderCompleteModal({ product, purchaseQuantity, items }) {
+export function OrderCompleteModal({ product, purchaseQuantity, itemArr }) {
   const dispatch = useDispatch();
   const { closeModal } = useModal();
+  const { id, name, price, description, quantity, category, product_image } = product
+ //the correct product quantity after order is complete
+ const updateProduct = { id, name, price, description, quantity, category }
+//  console.log('******************  Product******************', product);
+//  console.log('****************** before Product******************', updateProduct);
+ updateProduct.quantity = updateProduct.quantity - purchaseQuantity
+ updateProduct.url = product_image[0] //has to match what the fetchUpdateProduct key and data formate it wants
+//  console.log('******************purchaseQuantity******************', purchaseQuantity);
+//  console.log('******************updatedProduct******************', updateProduct);
 
-  const orderComplete = (e) => {
+
+  const onSubmit = (e) => {
     e.preventDefault();
     dispatch(deleteItemThunk(product.id)); //remove from shopping cart
     dispatch(fetchUpdateProduct(updateProduct)) //update product quantity
