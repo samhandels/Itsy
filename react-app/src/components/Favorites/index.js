ae282e6bbbe7697e60e4beb7e75e72d499e3cd24
Sam Handelsman <samhandelsman@gmail.com>
2023-09-01 11:44:33 -0700

    updated categories redirect on favorites page

--- react-app/src/components/Favorites/index.js
+++ react-app/src/components/Favorites/index.js
@@ -4,14 +4,23 @@ import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { ProductCard } from "../ProductCard";
 import { getAllFavorites } from '../../store/favoritesReducer';
+import { useHistory } from 'react-router-dom';
+
 
 const FavoritesPage = () => {
     const currentUser = useSelector((state) => state.session.user);
     const favorites = useSelector((state) => state.favorites.favorites);
     const dispatch = useDispatch()
+    const history = useHistory();
+
 
     const [filter, setFilter] = useState("")
     // console.log("favorites -------------", favorites)
+
+    const handleCategoryRedirect = (category) => {
+        history.push(`/products/search/${category}`);
+    }
+
 
     let favArr = Object.values(favorites)
     // console.log("fav Arr ---------------", favArr)
@@ -45,7 +54,7 @@ const FavoritesPage = () => {
     <div className='suggested-categories-fav'>
         <div id='category-title-Favorites'>Categories we think you'll love</div>
         <div className='Collection-Categories-fav'>
-            <div className='Collection-Category-Outer-Div' onClick="">
+            <div className='Collection-Category-Outer-Div' onClick={() => handleCategoryRedirect('Toys')}>
                 <div className='Photo-Div'>
                     <div className='Photo-Div-Left'>
                         <img id="collection-image" className='LeftUpper-Photo' src='https://i.etsystatic.com/9433423/r/il/d4a8bb/2857074970/il_794xN.2857074970_es2x.jpg'></img>
@@ -60,7 +69,7 @@ const FavoritesPage = () => {
                 <h3>Toys</h3>
             </div>
 
-            <div className='Collection-Category-Outer-Div'>
+            <div className='Collection-Category-Outer-Div' onClick={() => handleCategoryRedirect('Gifts')}>
                 <div className='Photo-Div'>
                     <div className='Photo-Div-Left'>
                         <img id="collection-image" className='LeftUpper-Photo' src='https://i.etsystatic.com/6193770/c/1091/867/0/125/il/841cd5/4423926777/il_340x270.4423926777_3lzq.jpg'></img>
@@ -75,7 +84,7 @@ const FavoritesPage = () => {
                 <h3>Gifts</h3>
             </div>
 
-            <div className='Collection-Category-Outer-Div'>
+            <div className='Collection-Category-Outer-Div' onClick={() => handleCategoryRedirect('Home')}>
                 <div className='Photo-Div'>
                     <div className='Photo-Div-Left'>
                         <img id="collection-image" className='LeftUpper-Photo' src='https://i.etsystatic.com/8950379/r/il/fab0f8/2291039261/il_340x270.2291039261_n4s0.jpg'></img>
@@ -90,7 +99,7 @@ const FavoritesPage = () => {
                 <h3>Home</h3>
             </div>
 
-            <div className='Collection-Category-Outer-Div'>
+            <div className='Collection-Category-Outer-Div' onClick={() => handleCategoryRedirect('Crafts')}>
                 <div className='Photo-Div'>
                     <div className='Photo-Div-Left'>
                         <img id="collection-image" className='LeftUpper-Photo' src='https://i.etsystatic.com/12316004/r/il/ec4a06/3824920921/il_340x270.3824920921_1i8f.jpg'></img>
@@ -139,7 +148,7 @@ const FavoritesPage = () => {
             <div className='suggested-categories-fav'>
                 <h1>Categories we think you'll love</h1>
                 <div className='Collection-Categories-fav'>
-                    <div className='Collection-Category-Outer-Div'>
+                    <div className='Collection-Category-Outer-Div' onClick={() => handleCategoryRedirect('Toys')}>
                         <div className='Photo-Div'>
                             <div className='Photo-Div-Left'>
                                 <img id="collection-image" className='LeftUpper-Photo' src='https://i.etsystatic.com/9433423/r/il/d4a8bb/2857074970/il_794xN.2857074970_es2x.jpg'></img>
@@ -154,7 +163,7 @@ const FavoritesPage = () => {
                         <h3>Toys</h3>
                     </div>
 
-                    <div className='Collection-Category-Outer-Div'>
+                    <div className='Collection-Category-Outer-Div' onClick={() => handleCategoryRedirect('Gifts')}>
                         <div className='Photo-Div'>
                             <div className='Photo-Div-Left'>
                                 <img id="collection-image" className='LeftUpper-Photo' src='https://i.etsystatic.com/6193770/c/1091/867/0/125/il/841cd5/4423926777/il_340x270.4423926777_3lzq.jpg'></img>
@@ -171,7 +180,7 @@ const FavoritesPage = () => {
 
                     <div className='Collection-Category-Outer-Div'>
                         <div className='Photo-Div'>
-                            <div className='Photo-Div-Left'>
+                            <div className='Photo-Div-Left' onClick={() => handleCategoryRedirect('Home')}>
                                 <img id="collection-image" className='LeftUpper-Photo' src='https://i.etsystatic.com/8950379/r/il/fab0f8/2291039261/il_340x270.2291039261_n4s0.jpg'></img>
                                 <img id="collection-image" className='LeftLower-Photo' src='https://i.etsystatic.com/11578147/r/il/e0d6a6/3779621232/il_340x270.3779621232_5x47.jpg'></img>
                             </div>
@@ -184,7 +193,7 @@ const FavoritesPage = () => {
                         <h3>Home</h3>
                     </div>
 
-                    <div className='Collection-Category-Outer-Div'>
+                    <div className='Collection-Category-Outer-Div' onClick={() => handleCategoryRedirect('Crafts')}>
                         <div className='Photo-Div'>
                             <div className='Photo-Div-Left'>
                                 <img id="collection-image" className='LeftUpper-Photo' src='https://i.etsystatic.com/12316004/r/il/ec4a06/3824920921/il_340x270.3824920921_1i8f.jpg'></img>
